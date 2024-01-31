57e50a3a063ddd52b6a0d244b303c7c419d48614
Ian <iankosgey535@gmail.com>
2024-01-31 16:50:43 +0300

    Handled event

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,15 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+function EyesOnMe() {
+    const handleBlur = () => {
+        console.log('Hey! Eyes on me!')
+    }
+  return (
+    <div>
+        <button onFocus={() => console.log('Good!')}  onBlur={handleBlur}>Eyes on me</button>
+    </div>
+  )
+}
+
+export default EyesOnMe;
